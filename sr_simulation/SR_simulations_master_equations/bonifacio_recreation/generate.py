22e961405d5f6e6d5f36e66f6c02d8d2c222c823
RoyCohen-github <59228397+RoyCohen-github@users.noreply.github.com>
2021-08-21 23:51:02 +0300

    Add files via upload

---
+++ sr_simulation/SR_simulations_master_equations/bonifacio_recreation/generate.py
@@ -0,0 +1,18 @@
+from SR_simmulations.SR_system import *
+
+N = 200
+
+SR_m_max = SR_system(N * SR_system.spin, m0=N * SR_system.spin, N=N)
+
+ntu = 30
+SR_m_max.creat_time_evolution(ntu * SR_m_max.natural_time_unit)
+
+# t is in natural time units
+t_in_ntu = [2.65, 3.97, 5.3, 6.62, 7.95]
+for t in t_in_ntu:
+    SR_m_max.plot_Pm(t * SR_m_max.natural_time_unit)
+plt.savefig(f'bonifacio_Fig_1_recreation.svg')
+plt.close()
+
+a = 1 + 1
+a = a + 1
