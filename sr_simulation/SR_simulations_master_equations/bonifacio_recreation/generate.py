cfedc38ac0929a0f618c305e75035f3cdc407cae
RoyCohen-github <59228397+RoyCohen-github@users.noreply.github.com>
2021-08-26 10:33:05 +0300

    Update generate.py

--- sr_simulation/SR_simulations_master_equations/bonifacio_recreation/generate.py
+++ sr_simulation/SR_simulations_master_equations/bonifacio_recreation/generate.py
@@ -5,7 +5,7 @@ N = 200
 SR_m_max = SR_system(N * SR_system.spin, m0=N * SR_system.spin, N=N)
 
 ntu = 30
-SR_m_max.creat_time_evolution(ntu * SR_m_max.natural_time_unit)
+SR_m_max.create_time_evolution(ntu * SR_m_max.natural_time_unit)
 
 # t is in natural time units
 t_in_ntu = [2.65, 3.97, 5.3, 6.62, 7.95]
