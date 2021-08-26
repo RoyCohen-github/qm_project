477ae95e72ec7e3e77d2d2afae8a883d476bad85
RoyCohen-github <59228397+RoyCohen-github@users.noreply.github.com>
2021-08-26 10:29:57 +0300

    Update generate.py

--- sr_simulation/SR_simulations_master_equations/final_sims/generate.py
+++ sr_simulation/SR_simulations_master_equations/final_sims/generate.py
@@ -8,10 +8,10 @@ CL_m_max = CL_system(N * SR_system.spin, m0=N * SR_system.spin, N=N)
 CL_m_0 = CL_system(N * SR_system.spin, m0=0, N=N)
 
 ntu = 30
-SR_m_max.creat_time_evolution(ntu * SR_m_max.natural_time_unit)
-SR_m_0.creat_time_evolution(ntu * SR_m_0.natural_time_unit)
-CL_m_max.creat_time_evolution(20 * ntu * CL_m_max.natural_time_unit)
-CL_m_0.creat_time_evolution(20 * ntu * CL_m_0.natural_time_unit)
+SR_m_max.create_time_evolution(ntu * SR_m_max.natural_time_unit)
+SR_m_0.create_time_evolution(ntu * SR_m_0.natural_time_unit)
+CL_m_max.create_time_evolution(20 * ntu * CL_m_max.natural_time_unit)
+CL_m_0.create_time_evolution(20 * ntu * CL_m_0.natural_time_unit)
 
 
 ##########################################################################################
